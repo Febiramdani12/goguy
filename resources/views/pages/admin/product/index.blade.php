539ba46599346df0c46be62a5dea998aa7596c97
Febi <mardiansy@gmail.com>
2021-09-06 21:33:20 +0700

    o

--- resources/views/pages/admin/product/index.blade.php
+++ resources/views/pages/admin/product/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('title')
-Jasa
+Dashboard Jasa
 @endsection
 
 @section('content')
--- resources/views/pages/admin/trx.blade.php
+++ resources/views/pages/admin/trx.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('title')
-    Dashboard Detail Transaksi
+    Dashboard Transaksi
 @endsection
 
 @section('content') 
